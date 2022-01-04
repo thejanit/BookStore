0eb096940b15ea786d2147a6e767cdae2930863c
Janit Srivastava <janitjanit1999@gmail.com>
2022-01-04 20:09:18 +0530

    Add files via upload

---
+++ books/form.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import fields, models
+from books.models import Review
+
+class ReviewForm(forms.ModelForm):
+    body = forms.CharField(widget=forms.Textarea(attrs={
+                           'class': "border rounded p-2 w-full", 'placeholder': "Write your review here..."}))
+    image = forms.ImageField(required=False)
+
+    class Meta:
+        model = Review
+        fields = ['body', 'image']
